ddefce385f7adccb6104f4635a4f5279eb630ff4
pisha <pisha@hotmail.fr>
2020-09-16 17:17:39 +0200

    ajout des variables et fonctions pour scene 2 et 3

--- app.js
+++ app.js
@@ -18,3 +18,35 @@ var scene = new ScrollMagic.Scene({
 })
 .addTo(controller);
 
+var scene2 = new ScrollMagic.Scene({
+
+    triggerElement: '.box2',
+    reverse : false
+
+})
+
+/* changer la class de box2 au scrolling */
+.setClassToggle('.box2', 'fade-in')
+.addIndicators({
+    name: 'INDICATIONS',
+    indent: 200,
+    colorStart: '#fff'
+})
+.addTo(controller);
+
+var scene3 = new ScrollMagic.Scene({
+
+    triggerElement: '.box3',
+    reverse : false
+
+})
+
+/* changer la class de box3 au scrolling */
+.setClassToggle('.box3', 'fade-in')
+.addIndicators({
+    name: 'INDICATIONS',
+    indent: 200,
+    colorStart: '#fff'
+})
+.addTo(controller);
+
